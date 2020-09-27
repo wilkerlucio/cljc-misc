68e9c49fa8de044f0d637de7cb3cba77747d683f
Wilker Lucio <wilkerlucio@gmail.com>
2020-09-27 13:24:46 -0300

    Add project.clj

---
+++ project.clj
@@ -0,0 +1,17 @@
+(defproject com.wsscode/cljc-misc "0.0.1"
+  :description "Collection of general utils for Clojure and Clojurescript."
+  :url "https://github.com/wilkerlucio/cljc-misc"
+  :license {:name "MIT"
+            :url  "https://opensource.org/licenses/MIT"}
+
+  :source-paths ["src"]
+
+  :dependencies [[org.clojure/clojure "1.10.0" :scope "provided"]
+                 [org.clojure/clojurescript "1.10.764" :scope "provided"]]
+
+  :jar-exclusions [#"resources/.*" #"node-modules/.+"]
+
+  :deploy-repositories [["clojars" {:url   "https://clojars.org/repo/"
+                                    :creds :gpg :checksum :ignore}]
+                        ["releases" :clojars]
+                        ["snapshots" :clojars]])
