df67a267455df0e2584854c225e951094a6d7f5c
Wilker Lucio <wilkerlucio@gmail.com>
2020-12-21 13:27:33 -0300

    BUmp and changelog 2012.12.21

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,7 @@
 # Changelog
 
+## 2020.12.21
+- Add `sleep-ms` helper to `time` namespace
+
 ## 2020.12.01
 - Add `restore-order` helper
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.wsscode/cljc-misc "2020.12.01"
+(defproject com.wsscode/cljc-misc "2020.12.21"
   :description "Collection of general utils for Clojure and Clojurescript."
   :url "https://github.com/wilkerlucio/cljc-misc"
   :license {:name "MIT"
