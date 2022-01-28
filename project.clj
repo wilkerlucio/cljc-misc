d164f09c427e35a7e3114d6e0839c21e9d7f5d53
Wilker Lucio <wilkerlucio@gmail.com>
2022-01-28 15:04:43 -0300

    Bump 2022.01.28

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.wsscode/cljc-misc "2022.01.25"
+(defproject com.wsscode/cljc-misc "2022.01.28"
   :description "Collection of general utils for Clojure and Clojurescript."
   :url "https://github.com/wilkerlucio/cljc-misc"
   :license {:name "MIT"
